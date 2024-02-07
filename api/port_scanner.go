895e9e817cebb452597123728d064e4d124d09d6
jMax <jaylee.watcher@gmail.com>
2024-02-07 23:28:21 +0700

    feat(api): scan port status

---
+++ api/port_scanner.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func PortScanner(protocol, hostname string, start, end int) {
+	for i := start; i < end; i++ {
+		port := strconv.FormatInt(int64(i), 10)
+		conn, err := net.Dial(protocol, hostname+":"+port)
+		if err == nil {
+			fmt.Println("Port", i, "open")
+			conn.Close()
+		} else {
+			fmt.Println("Port", i, "closed")
+		}
+	}
+}
+
+func PortChecker(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
